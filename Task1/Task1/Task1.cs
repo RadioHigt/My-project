31fd83e5101a733aa8966eedf081479dec7b79e5
RadioHigt <115972036+RadioHigt@users.noreply.github.com>
2023-03-27 10:00:42 +0700

    Add files via upload

---
+++ Task1/Task1/Task1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+class HelloWorld
+{
+    static void Main()
+    {
+        //  //task1.1 
+        //   int N = 0;
+        //   string S = Console.ReadLine();
+        //   string J = Console.ReadLine();
+        //   for(int i = 0; i < J.Length; i++)
+        //   {
+        //       bool check = false;
+        //       for(int j = 0; j < S.Length; j++)
+        //       {
+        //           if(J[i]==S[j])
+        //           {
+        //             check = true;
+        //             break;
+        //           }
+        //       }
+        //       if(check)
+        //       {
+        //           N++;
+        //       }
+        //   }
+        //   Console.WriteLine(N);
+        // //task1.3
+        // int N=10;
+        // bool check=false;
+        // List<int> nums = new List<int>();
+        // for(int i = 0; i < N; i++)
+        // {
+        //     int a = int.Parse(Console.ReadLine());
+        //     nums.Add(a);
+        // }
+        // for(int i = 0; i < N; i++)
+        // {
+        //     for(int j = 0; j < N; j++)
+        //     {
+        //         if(nums[i]==nums[j] & i!=j)
+        //         {
+        //             check=true;
+        //             break;
+        //         }
+        //     }
+        //     if(check)
+        //     {
+        //         break;
+        //     }
+        // }
+        // Console.WriteLine(check);
+    }
+}
